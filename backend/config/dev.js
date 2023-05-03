7149bb8eadc0439aa4845406cf4993e91f2c4bd7
OritLeshem <azmono@gmail.com>
2023-05-03 12:09:22 -0400

    connect db

--- backend/config/dev.js
+++ backend/config/dev.js
@@ -1,4 +1,6 @@
 module.exports = {
-  dbURL: 'mongodb://127.0.0.1:27017',
-  dbName : 'tester_db'
+  // dbURL: 'mongodb://127.0.0.1:27017',
+  // dbName: 'tester_db'
+  dbURL: 'mongodb+srv://admin:1234@cluster0.5hqlzet.mongodb.net/test',
+  dbName: 'picnic'
 }
--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,4 +1,6 @@
 module.exports = {
-  dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-  dbName : 'tester_db'
+  // dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  // dbName : 'tester_db'
+  dbURL: 'mongodb+srv://admin:1234@cluster0.5hqlzet.mongodb.net/test',
+  dbName: 'picnic'
 }
